7b4f0131d16f1ca3682a0814aff71df5cb8b3dfa
Guy Jara <guy_jara@yahoo.com>
2026-03-19 23:17:26 -0600

    Removed SQL files to create ADD PARITION, now that automated process is working.

--- sql/10_fixed/101_raw_swing_bed_snf_data_oct2024_fixed_add_partition.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE healthcare_catalog_db.raw_swing_bed_snf_data_oct2024_fixed
-ADD IF NOT EXISTS
-PARTITION (ingest_dt='{{INGEST_DT}}')
-LOCATION 's3://healthcare-data-lake-gj/raw/swing_bed_snf_data_oct2024/ingest_dt={{INGEST_DT}}/';
--- sql/90_checks/900_dim_facility_check.sql
+++
@@ -1,3 +0,0 @@
-SELECT
-  count(*) - count(distinct ccn_provnum) AS dupes
-FROM healthcare_curated_db.dim_facility;
